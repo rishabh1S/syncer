5b8049b6cab626cd5c001a93679393ab13dcb385
Rishabh Singh <89694788+SinghRishabh19@users.noreply.github.com>
2022-10-23 16:36:25 +0530

    in

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import { Fab, Zoom } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+
 
 function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -26,23 +32,33 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setExpanded(true);
+  }
   return (
     <div>
       <form className="create-note">
-        <input
+        {isExpanded && (<input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
         />
+        )}
+
         <textarea
+          onClick={expand}
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import DescriptionTwoToneIcon from '@mui/icons-material/DescriptionTwoTone';
 
 function Header() {
   return (
     <header>
-      <h1>Notes Sync</h1>
+      <h1> <DescriptionTwoToneIcon/> Syncer</h1>
     </header>
   );
 }
